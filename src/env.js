ba73f76b88d3b36b3d3704c4f83b0c964f3da1aa
romasmv <rmorozovas@gmail.com>
2025-06-30 11:11:32 +0300

    up

--- src/env.js
+++ src/env.js
@@ -1,15 +1,23 @@
 
-const argList = process.argv.slice(2)
+import dotenv from 'dotenv';
+
+const argList = process.argv.slice(2);
 const args = {};
 
-for(const str of argList){
-    const [key,value] = str.split('=');
+for (const str of argList) {
+    const [key, value] = str.split('=');
 
-    if(key && value && key.startsWith('--'))  {
-        args[key.slice(2)]=value;
-        
+    if (key && value && key.startsWith('--')) {
+        args[key.slice(2)] = value;
     }
 }
-console.log(args);
 
-export const PORT = 5517;
+dotenv.config({
+    path: 'src/.env.' + args.env,
+});
+
+export const PORT = +process.env.PORT ?? 5517;
+export const TITLE = process.env.TITLE ?? 'Project title';
+export const DB_DATABASE = process.env.DB_DATABASE ?? 'test_db';
+export const DB_USER = process.env.DB_USER ?? 'test_user';
+export const DB_PASSWORD = process.env.DB_PASSWORD ?? 'test_password';
