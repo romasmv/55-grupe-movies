3bfa33f16457413a3a6556bd227ade4a6455b06b
romasmv <rmorozovas@gmail.com>
2025-07-03 11:17:17 +0300

    up

--- src/api/public/postRegister.js
+++ src/api/public/postRegister.js
@@ -1,4 +1,32 @@
-export function postRegister(req, res) {
+import { connection } from "../../db.js";
+import { IsValid } from "../../lib/IsValid.js";
+
+export async function postRegister(req, res) {
+    const [err, msg] = IsValid.fields(req.body, {
+        username: 'username',
+        email: 'email',
+        password: 'password',
+        tos: 'tos',
+    });
+
+    if (err) {
+        return res.json({
+            status: 'error',
+            msg: msg,
+        });
+    }
+
+    const { username, email, password } = req.body;
+
+    try {
+        const sql = `INSERT INTO users (username, email, password) VALUES (?, ?, ?);`;
+        const response = await connection.execute(sql, [username, email, password]);
+
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+    }
+
     return res.json({
         status: 'success',
         msg: 'Sekminga registracija',
--- src/routes/publicRouter.js
+++ src/routes/publicRouter.js
@@ -7,22 +7,11 @@ import { PageRegister } from '../pages/Register.js';
 
 export const publicRouter = express.Router();
 
-publicRouter.get('/', (req, res) => {
-    return res.send(new PageHome().render());
-});
+publicRouter.get('/', (req, res) => res.send(new PageHome().render()));
 
-publicRouter.get('/movies', (req, res) => {
-    return res.send(new PageMovies().render());
-});
+publicRouter.get('/movies', (req, res) => res.send(new PageMovies().render()));
 
-publicRouter.get('/categories', (req, res) => {
-    return res.send(new PageCategories().render());
-});
+publicRouter.get('/categories', (req, res) => res.send(new PageCategories().render()));
 
-publicRouter.get('/login', (req, res) => {
-    return res.send(new PageLogin().render());
-});
-
-publicRouter.get('/register', (req, res) => {
-    return res.send(new PageRegister().render());
-});
+publicRouter.get('/login', (req, res) => res.send(new PageLogin().render()));
+publicRouter.get('/register', (req, res) => res.send(new PageRegister().render()));
