15dbddb81245d751c8d69af40dcd9c6aacbfceab
romasmv <rmorozovas@gmail.com>
2025-07-16 11:30:55 +0300

    up

--- src/routes/adminApiRouter.js
+++ src/routes/adminApiRouter.js
@@ -1,10 +1,13 @@
 import express from 'express';
-import { postCategories } from '../api/admin/postCategories.js';
-import { deleteCategories } from '../api/admin/deleteCategories.js';
-import { putCategories } from '../api/admin/putCategories.js';
+import { postCategories } from '../api/admin/categories/postCategories.js';
+import { deleteCategories } from '../api/admin/categories/deleteCategories.js';
+import { putCategories } from '../api/admin/categories/putCategories.js';
+import { postMovies } from '../api/admin/movies/postMovies.js';
 
 export const adminApiRouter = express.Router();
 
 adminApiRouter.post('/categories', postCategories);
 adminApiRouter.put('/categories/:original_url', putCategories);
 adminApiRouter.delete('/categories/:url', deleteCategories);
+
+adminApiRouter.post('/movies', postMovies);
