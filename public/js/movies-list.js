7cbaaa2a7a127645fb8224eefe5c13ccddeeee82
romasmv <rmorozovas@gmail.com>
2025-07-17 12:37:37 +0300

    update

---
+++ public/js/movies-list.js
@@ -0,0 +1,16 @@
+const deleteButtonsDOM = document.querySelectorAll('table button');
+
+for (const btnDOM of deleteButtonsDOM) {
+    btnDOM.addEventListener('click', () => {
+        fetch('/api/admin/movies/' + btnDOM.dataset.url, {
+            method: 'DELETE',
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.status === 'success') {
+                    btnDOM.closest('tr').remove();
+                }
+            })
+            .catch(console.error);
+    });
+}
---
+++ src/db/admin/getAllMovies.js
@@ -0,0 +1,20 @@
+import { connection } from "../../db.js";
+
+export async function getAllMovies() {
+    try {
+        const sql = `
+            SELECT
+                movies.*,
+                general_status.name AS statusName,
+                categories.title AS categoryName
+            FROM movies
+            INNER JOIN general_status
+                ON movies.status_id = general_status.id
+            LEFT JOIN categories
+                ON movies.category_id = categories.id;`;
+        const [result] = await connection.execute(sql);
+        return result;
+    } catch (err) {
+        return [];
+    }
+}
